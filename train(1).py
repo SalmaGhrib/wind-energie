2fcca504cf9d425c5399eb2b0a2a53484569af3c
SalmaGhrib <100800934+SalmaGhrib@users.noreply.github.com>
2026-09-23 00:46:57 +0200

    Update comment for retraining test in train(1).py

--- train(1).py
+++ train(1).py
@@ -1,5 +1,5 @@
 """Regression : prediction des kWh produits en une heure par une eolienne fictive.
-# test de reentraianament 2
+# test de reentraianament 262
 python train.py --train-data donnees_A/train.csv --val-data donnees_A/val.csv
 """
 import argparse
