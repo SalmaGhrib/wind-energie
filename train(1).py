2f1ee30971d1325bc2267f1f8a65292260c3d1ed
SalmaGhrib <100800934+SalmaGhrib@users.noreply.github.com>
2026-09-23 00:41:22 +0200

    Add retraining test comment in train(1).py
    Added a comment for retraining test in the script.

--- train(1).py
+++ train(1).py
@@ -1,5 +1,5 @@
 """Regression : prediction des kWh produits en une heure par une eolienne fictive.
-
+# test de reentraianament 
 python train.py --train-data donnees_A/train.csv --val-data donnees_A/val.csv
 """
 import argparse
